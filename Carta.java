7831de0f275f113ef187f109e0e020f9d1028bb6
CF-Leonardo <leonardoarchetti@gmail.com>
2016-05-17 11:12:16 +0200

    aggiungo commenti per javadoc

--- Carta.java
+++ Carta.java
@@ -7,7 +7,7 @@ public class Carta {
 	/**
 	 * Costruttore del tipo Carta
 	 * @param _indexSeme numero tra 0 e 3-> cuori, quadri, fiori, picche
-	 * @param _indexNumero
+	 * @param _indexNumero valore della carta-> da 0 a 12
 	 */
 	public Carta(int _indexSeme,int _indexNumero)
 	{
@@ -16,8 +16,8 @@ public class Carta {
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Restituisce il valore della carta
+	 * @return un numero_> 14=asse,2,3,4,5,6,7,8,9,10,11=jack,12=donna,13=re
 	 */
 	public int getValore()
 	{
@@ -26,8 +26,8 @@ public class Carta {
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Restituisce il nome della carta
+	 * @return stringa "asso", "due", "tre", "quattro", "cinque", "sei", "sette", "otto", "nove", "dieci", "jack", "donna", "re"
 	 */
 	public String getNome()
 	{
