8943474f6b6fcc6537a75ca9dfb5c02e484eebf0
CF-Ivan <ivan.archetti@gmail.com>
2016-05-17 11:03:52 +0200

    Update Carta.java

--- Carta.java
+++ Carta.java
@@ -1,24 +1,37 @@
 package CartaPiùAlta;
 
-/**
- * 
- * 
- **/
 public class Carta {
-	private char segno;
-	private int valore;
+	int indexSeme;
+	int indexNumero;
 	
-public Carta(char _segno,int _valore)
-{
-	segno=_segno;
-	valore=_valore;
-}
-public int getValore()
-{
-	return valore;
-}
-public char getSegno()
-{
-	return segno;
-}
+	/**
+	 * 
+	 * @param _indexSeme
+	 * @param _indexNumero
+	 */
+	public Carta(int _indexSeme,int _indexNumero)
+	{
+		indexSeme=_indexSeme;
+		indexNumero=_indexNumero;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getValore()
+	{
+		return TipoCarta.getValore(indexNumero);
+				//////TipoCarta.VAL_CARTE[indexNumero];
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getNome()
+	{
+		return  TipoCarta.getNome(indexNumero)+" di "+TipoCarta.getSeme(indexSeme);
+				//////TipoCarta.NOMECARTE[indexNumero]+" di "+TipoCarta.SEME[indexSeme];
+	}
 }
