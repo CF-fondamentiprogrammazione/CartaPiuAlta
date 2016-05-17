ba3d271ec75e8e485c382381eea02fcb8dd1ebc5
CF-Ivan <ivan.archetti@gmail.com>
2016-05-17 11:13:53 +0200

    Update Giocatore.java

--- Giocatore.java
+++ Giocatore.java
@@ -1,28 +1,54 @@
-package CartaPiùAlta;
+package CartaPiÃ¹Alta;
 
 public class Giocatore {
-private int amount;
-private String nome;
-public void setAmount(int vincita)
-{
-	amount=amount+vincita;
-}
-public Giocatore( String _nome,int _amount)
-{
-	nome=_nome;
-	amount=_amount;
-}
-public boolean puòGiocare()
-{
-	if(amount>0)
-		return true; else return false;
-}
-public int getAmount()
-{
-	return amount;
-}
-public String getNome()
-{
-	return nome;
-}
+	private int amount;
+	private String nome;
+	
+	/**
+	 * 
+	 * @param _nome
+	 * @param _amount
+	 */
+	public Giocatore( String _nome,int _amount)
+	{
+		nome=_nome;
+		amount=_amount;
+	}
+	
+	/**
+	 * 
+	 * @param vincita
+	 */
+	public void setAmount(int vincita)
+	{
+		amount+=vincita;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean puÃ²Giocare()
+	{
+		if(amount>0)return true; 
+		else return false;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getAmount()
+	{
+		return amount;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getNome()
+	{
+		return nome;
+	}
 }
